0b5364f2ce111c27e889106b37811a70d88b125f
Max Cobb <maxxc@mac.com>
2021-04-01 10:58:05 +0100

    Added SwiftUI Sample code for Audio Channels

---
+++ SwiftUI-Example/SwiftUI-Audio/ContentView.swift
@@ -0,0 +1,154 @@
+//
+//  ContentView.swift
+//  SwiftUI-Audio
+//
+//  Created by Max Cobb on 25/03/2021.
+//
+
+import SwiftUI
+import AgoraRtmKit
+import AgoraRtcKit
+
+struct ContentView: View {
+    @State var joinedChannel: Bool = false
+    var rtckit: AgoraRtcEngineKit { self.lvm.rtckit }
+    var rtmkit: AgoraRtmKit? { self.lvm.rtmkit }
+    @ObservedObject var lvm = AgoraObservable()
+    var body: some View {
+        Form {
+            Section(header: Text("Channel Information")) {
+                TextField(
+                    "Channel Name", text: $lvm.channelName
+                ).disabled(joinedChannel)
+                TextField(
+                    "Username", text: $lvm.username
+                ).disabled(joinedChannel)
+            }
+            Button(action: {
+                joinedChannel.toggle()
+                if !joinedChannel {
+                    self.lvm.members.removeAll()
+                    rtckit.leaveChannel()
+                    rtmkit?.logout()
+                    self.lvm.rtmIsLoggedIn = false
+                } else {
+                    self.lvm.joinChannel()
+                }
+            }, label: {
+                Text("\(joinedChannel ? "Leave" : "Join") Channel")
+                    .accentColor(joinedChannel ? .red : .blue)
+            })
+            if joinedChannel {
+                Section(header: Text("Members")) {
+                    List(lvm.members, id: \.self) { string in
+                        Text(string)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct UserData: Codable {
+    var rtmId: String
+    var rtcId: UInt
+    var username: String
+}
+
+class AgoraObservable: NSObject, ObservableObject {
+    @Published var channelName: String = ""
+    @Published var username: String = ""
+    @Published var members: [String] = []
+    @Published var membersLookup: [String: (rtcId: UInt, username: String)] = [:] {
+        didSet {
+            print("did set then")
+            members = self.membersLookup.values.compactMap { $0.username + ($0.username == username ? " (Me)" : "") }
+        }
+    }
+
+    var rtcId: UInt = 0
+    var rtmId = UUID().uuidString
+    var channel: AgoraRtmChannel?
+    var rtmIsLoggedIn = false
+
+    lazy var rtckit: AgoraRtcEngineKit = {
+        let engine = AgoraRtcEngineKit.sharedEngine(
+            withAppId: <#Agora App ID#>, delegate: nil
+        )
+        engine.setChannelProfile(.liveBroadcasting)
+        return engine
+    }()
+
+    lazy var rtmkit: AgoraRtmKit? = {
+        let rtm = AgoraRtmKit(
+            appId: <#Agora App ID#>, delegate: self
+        )
+        return rtm
+    }()
+
+    func joinChannel() {
+        if !self.rtmIsLoggedIn {
+            rtmkit?.login(byToken: nil, user: self.rtmId) { rtmLoggedIn in
+                self.rtmIsLoggedIn = true
+                self.joinChannel()
+            }
+            return
+        }
+        self.rtckit.joinChannel(byToken: nil, channelId: self.channelName, info: nil, uid: self.rtcId) { (channel, uid, errCode) in
+            self.rtcId = uid
+            self.channel = self.rtmkit?.createChannel(withId: self.channelName, delegate: self)
+            self.channel?.join(completion: { joinStatus in
+                if joinStatus == .channelErrorOk {
+                    let user = UserData(rtmId: self.rtmId, rtcId: self.rtcId, username: self.username)
+                    guard let jsonData = try? JSONEncoder().encode(user),
+                          let jsonString = String(data: jsonData, encoding: .utf8) else {
+                        return
+                    }
+                    self.membersLookup[user.rtmId] = (user.rtcId, user.username)
+                    self.channel?.send(AgoraRtmMessage(text: jsonString))
+                } else {
+                    self.channel = nil
+                }
+            })
+        }
+    }
+    func sendUsername(to member: AgoraRtmMember) {
+        let user = UserData(rtmId: self.rtmId, rtcId: self.rtcId, username: self.username)
+        guard let jsonData = try? JSONEncoder().encode(user),
+              let jsonString = String(data: jsonData, encoding: .utf8) else {
+            return
+        }
+        self.rtmkit?.send(AgoraRtmMessage(text: jsonString), toPeer: member.userId)
+   }
+}
+
+extension AgoraObservable: AgoraRtmChannelDelegate, AgoraRtmDelegate {
+    func channel(_ channel: AgoraRtmChannel, memberJoined member: AgoraRtmMember) {
+        self.sendUsername(to: member)
+    }
+
+    func channel(_ channel: AgoraRtmChannel, messageReceived message: AgoraRtmMessage, from member: AgoraRtmMember) {
+        parseMemberData(from: message.text)
+    }
+    func rtmKit(_ kit: AgoraRtmKit, messageReceived message: AgoraRtmMessage, fromPeer peerId: String) {
+        parseMemberData(from: message.text)
+    }
+
+    func parseMemberData(from text: String) {
+        guard let textData = text.data(using: .utf8),
+              let decodedUserData = try? JSONDecoder().decode(UserData.self, from: textData)
+        else {
+            return
+        }
+        membersLookup[decodedUserData.rtmId] = (decodedUserData.rtcId, decodedUserData.username)
+    }
+    func channel(_ channel: AgoraRtmChannel, memberLeft member: AgoraRtmMember) {
+        membersLookup.removeValue(forKey: member.userId)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Example/SwiftUI-Audio/SwiftUI_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AudioApp.swift
+//  SwiftUI-Audio
+//
+//  Created by Max Cobb on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
